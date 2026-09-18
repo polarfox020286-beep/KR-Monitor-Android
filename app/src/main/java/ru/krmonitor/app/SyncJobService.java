27f9b237efaa44ac6f1843867d5dbba8d0a4c6b9
polarfox020286-beep <polarfox020286@gmail.com>
2026-09-18 11:56:20 +0300

    Refresh UI after automatic KR scan

--- app/src/main/java/ru/krmonitor/app/SyncJobService.java
+++ app/src/main/java/ru/krmonitor/app/SyncJobService.java
@@ -4,7 +4,7 @@ import android.app.job.*;
 
 public class SyncJobService extends JobService {
     @Override public boolean onStartJob(JobParameters p) {
-        new Thread(() -> { SyncEngine.sync(getApplicationContext()); jobFinished(p,false); },"kr-sync").start();
+        new Thread(() -> {\n            SyncEngine.sync(getApplicationContext());\n            sendBroadcast(new android.content.Intent("ru.krmonitor.app.SYNC_COMPLETE").setPackage(getPackageName()));\n            jobFinished(p,false);\n        },"kr-sync").start();
         return true;
     }
     @Override public boolean onStopJob(JobParameters p) { return true; }
