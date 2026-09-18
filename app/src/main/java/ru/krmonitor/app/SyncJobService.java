f7de1a489c6c569d1c37e9503f45dde1721eec9d
polarfox020286-beep <polarfox020286@gmail.com>
2026-09-18 11:56:46 +0300

    Fix automatic sync completion broadcast

--- app/src/main/java/ru/krmonitor/app/SyncJobService.java
+++ app/src/main/java/ru/krmonitor/app/SyncJobService.java
@@ -4,7 +4,11 @@ import android.app.job.*;
 
 public class SyncJobService extends JobService {
     @Override public boolean onStartJob(JobParameters p) {
-        new Thread(() -> {\n            SyncEngine.sync(getApplicationContext());\n            sendBroadcast(new android.content.Intent("ru.krmonitor.app.SYNC_COMPLETE").setPackage(getPackageName()));\n            jobFinished(p,false);\n        },"kr-sync").start();
+        new Thread(() -> {
+            SyncEngine.sync(getApplicationContext());
+            sendBroadcast(new android.content.Intent("ru.krmonitor.app.SYNC_COMPLETE").setPackage(getPackageName()));
+            jobFinished(p,false);
+        },"kr-sync").start();
         return true;
     }
     @Override public boolean onStopJob(JobParameters p) { return true; }
